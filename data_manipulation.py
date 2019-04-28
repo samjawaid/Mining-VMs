4deffb183307eb9fad4fef9b4d290235b86a0077
samjawaid <samjawaid@ymail.com>
2019-04-28 17:48:18 -0500

    Update data_manipulation.py

--- data_manipulation.py
+++ data_manipulation.py
@@ -16,24 +16,25 @@ def createDisplayTxt():
         file1.write("\nBENCHMARK RESULTS\n")
         file1.write("\nTime To Launch(s): %s \n" % (lines[4]))
         file1.write("\nSTORAGE CHARACTERISTICS\n")
-        file1.write("Sysbench (Operations per Second): %s \n" % (lines[4]))
-        file1.write("Sequential read IOPs: %s \n" % (lines[5]))
-        file1.write("Sequential read Band width: %s \n" % (lines[6]))
-        file1.write("Sequential Write IOPs: %s \n" % (lines[7]))
-        file1.write("Sequential Write Band Width: %s \n" % (lines[8]))
-        file1.write("Random Read IOPs: %s \n" % (lines[9]))
-        file1.write("Random read Band Width: %s \n" % (lines[10]))
-        file1.write("Random Write IOPs: %s \n" % (lines[11]))
-        file1.write("Random Write Band Width: %s \n" % (lines[12]))
-        file1.write("Random Read/Write Read IOPs: %s \n" % (lines[13]))
-        file1.write("Random Read/Write Read Band Width: %s \n" % (lines[14]))
-        file1.write("Random Read/Write Write IOPs: %s \n" % (lines[15]))
-        file1.write("Random Read/Write Write Band Width: %s \n" % (lines[16]))
-        file1.write("\nCOMPUTE CHARACTERISTICS \n")
-        file1.write("RAM: \n")
+        file1.write("Sysbench (Operations per Second): %s \n" % (lines[5]))
+        file1.write("Sequential read IOPs: %s \n" % (lines[6]))
+        file1.write("Sequential read Bandwidth: %s \n" % (lines[7]))
+        file1.write("Sequential Write IOPs: %s \n" % (lines[8]))
+        file1.write("Sequential Write Bandwidth: %s \n" % (lines[9]))
         file1.write("\nNETWORK CHARACTERISTICS: \n")
-        file1.write("Throughput: \n")
-        file1.write("Date: 4/4/2019")#%s \n" % (lines[19]))
+        file1.write("Transfer (Sender): %s \n" % (lines[10]))
+        file1.write("Bandwidth (Sender): %s \n" % (lines[11]))
+        file1.write("Transfer (Receiver): %s \n" % (lines[12]))
+        file1.write("Bandwidth (Receiver): %s \n" % (lines[13]))
+        file1.write("\nCOMPUTE CHARACTERISTICS \n")
+        file1.write("Events per Second: %s \n" % (lines[14]))
+        file1.write("Average Latency: %s \n" % (lines[15]))
+        file1.write("Processor Frequency: %s \n" % (lines[16]))
+        file1.write("RAM Amount: %s \n" % (lines[17]))
+        file1.write("Average CPU Write Speed: %s \n" % (lines[18]))
+        file1.write("Average Usage Percent of CPU: %s \n" % (lines[19]))
+        
+#        file1.write("Date: 4/4/2019")#%s \n" % (lines[19]))
         file1.close()
         subprocess.Popen(['gedit ~/results.txt'], shell=True)
         #subprocess.Popen(['results.txt'], shell=True)
@@ -193,108 +194,144 @@ def parse_storage():
     SeqWBW = line84[23:27];
     text_file.write(SeqWBW)
 
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random read IOPs
-    line120 = lines[120];
-    RandRIOPs = line120[14:18];
-    text_file.write(RandRIOPs)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random read BW
-    RandRBW = line120[23:27];
-    text_file.write(RandRBW)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random write IOPS
-    line157 = lines[157];
-    RandWIOPs = line157[14:18];
-    text_file.write(RandWIOPs)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random write BW
-    RandWBW = line157[23:27];
-    text_file.write(RandWBW)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random read/write Read IOPS
-    line195 = lines[195];
-    RandRWreadIOPs = line195[14:18];
-    text_file.write(RandRWreadIOPs)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random (read/write) Read BW
-    RandRWreadBW = line195[23:27];
-    text_file.write(RandRWreadBW)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random read/write Write IOPS
-    line207 = lines[207];
-    RandRWwriteIOPs = line207[14:17];
-    text_file.write(RandRWwriteIOPs)
-
-    #comma
-    line37 = lines[37];
-    comma = line37[26];
-    text_file.write(comma)
-
-    #space
-    text_file.write(" ")
-
-    #random read/write Write BW
-    RandRWwriteBW = line207[22:26];
-    text_file.write(RandRWwriteBW)
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random read IOPs
+##    line120 = lines[120];
+##    RandRIOPs = line120[14:18];
+##    text_file.write(RandRIOPs)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random read BW
+##    RandRBW = line120[23:27];
+##    text_file.write(RandRBW)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random write IOPS
+##    line157 = lines[157];
+##    RandWIOPs = line157[14:18];
+##    text_file.write(RandWIOPs)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random write BW
+##    RandWBW = line157[23:27];
+##    text_file.write(RandWBW)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random read/write Read IOPS
+##    line195 = lines[195];
+##    RandRWreadIOPs = line195[14:18];
+##    text_file.write(RandRWreadIOPs)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random (read/write) Read BW
+##    RandRWreadBW = line195[23:27];
+##    text_file.write(RandRWreadBW)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random read/write Write IOPS
+##    line207 = lines[207];
+##    RandRWwriteIOPs = line207[14:17];
+##    text_file.write(RandRWwriteIOPs)
+##
+##    #comma
+##    line37 = lines[37];
+##    comma = line37[26];
+##    text_file.write(comma)
+##
+##    #space
+##    text_file.write(" ")
+##
+##    #random read/write Write BW
+##    RandRWwriteBW = line207[22:26];
+##    text_file.write(RandRWwriteBW)
 
     text_file.close()
 
-def parse_network():
-    #write to NetworkCSV.txt
-    pass
+def parse_network():    
+    
+#Python Script
+
+    Netlines = [] #Declare an empty list named "lines"
+    with open ('NetworkSSHOutput.txt', 'rt') as in_file:  #Open file lorem.txt for reading of text data.
+        for line in in_file: #For each line of text store in a string variable named "line", and
+            Netlines.append(line)  #add that line to our list of lines.
+
+    text_file = open("NetworkCSV.txt", "w")
+
+    #Sender Transfer Amount
+    lines15 = Netlines[15];
+    TransAmntS = lines15[25:29];
+    text_file.write(TransAmntS)
+
+    #comma
+    text_file.write(",")
+
+    #Sender Brandwidth Amount
+    TransAmntS = lines15[39:42];
+    text_file.write(TransAmntS)
+
+    #comma
+    text_file.write(",")
+
+    #Reciever Transfer Amount
+    lines16 = Netlines[16];
+    TransAmntR = lines16[25:29];
+    text_file.write(TransAmntR)
+
+    #comma
+    text_file.write(",")
+
+    #Reciever Brandwidth Amount
+    TransAmntR = lines16[39:42];
+    text_file.write(TransAmntR)
+
+    text_file.close()
